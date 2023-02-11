ebb6016cfe1ee977d473cfe63fe8b8054d99b9f9
dante01yoon <bunggl@naver.com>
2023-02-11 23:47:14 +0900

    feat: add client/* path static controller

--- api/src/api/app.controller.ts
+++ api/src/api/app.controller.ts
@@ -1,26 +1,22 @@
+import { } from "@dante/ssr";
 import { Controller, Get, Req, Res } from "@nestjs/common";
 import { FastifyReply, FastifyRequest } from "fastify";
 import * as fs from "fs";
 import { AppService } from "./app.service";
 import path = require("path");
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-
-  @Get("/main.js")
-  getMainJs(@Res() res:FastifyReply) {
-    const stream = fs.readFileSync(path.join(__dirname, '..','node_modules/@dante/ssr/dist/main.js'))
-    res.type("text/javascript").send(stream);
+  @Get("/client/*")
+  getMainJs(@Res() res:FastifyReply, @Req() req: FastifyRequest) {
+    const clientStaticPath = req.url.split("/client").at(-1);
+    const stream = fs.readFileSync(path.join(__dirname, '..',`node_modules/@dante/ssr/dist/${clientStaticPath}`))
+    res.send(stream);
   }
 
-  @Get("/main.mjs")
-  getMainMjs(@Res() res:FastifyReply) {
-    const stream = fs.readFileSync(path.join(__dirname, '..','node_modules/@dante/ssr/dist'))
-    res.type("text/javascript").send(stream);
-  }
-
-  @Get()
+  @Get("*")
   getIndex(@Req() req: FastifyRequest, @Res() res: FastifyReply) {
     // @ts-ignore
     console.log("res: ", res.raw.render)
